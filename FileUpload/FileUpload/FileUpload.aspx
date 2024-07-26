82d0b3f3eb6cda7be570956f9aa59240a8f49ad0
Vraj Clerk <129680505+vrajclerk@users.noreply.github.com>
2024-07-26 19:23:30 +0530

    FileUpload

---
+++ FileUpload/FileUpload/FileUpload.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUpload.aspx.cs" Inherits="FileUpload.FileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+             <br />
+             <br />
+            <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click" />
+
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FileUpload/FileUpload/FileUpload.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FileUpload
+{
+    public partial class FileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string fileExtension = Path.GetExtension(FileUpload1.FileName);
+                if (fileExtension.ToLower() != ".doc" && fileExtension.ToLower() != ".docx")
+                {
+
+                    Label1.Text = "Only files with .doc and .docx extension are allowed";
+                    Label1.ForeColor = System.Drawing.Color.Red;
+                }
+                else
+                {
+                    int fileSize = FileUpload1.PostedFile.ContentLength;
+                    if (fileSize > 2097152)
+                    {
+                        Label1.Text = "File size must not exceed 2MB";
+                        Label1.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        FileUpload1.SaveAs(Server.MapPath("~/Images/" + FileUpload1.FileName));
+                        Label1.Text = "File Uploaded Successfully";
+                        Label1.ForeColor = System.Drawing.Color.Green;
+                    }
+                }
+            }
+            else
+            {
+                Label1.Text= "Please Select File";
+                Label1.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+    }
+}
